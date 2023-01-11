5b5bdf4e42935346eb05ceed1ca762172204aca1
Pavel Rudzko <est1@inbox.ru>
2023-01-11 20:06:48 +0100

    first js task

---
+++ arraj.js
@@ -0,0 +1,25 @@
+"use strict"
+let array = [1,2,3,45,67,98,21,42,24,];
+let maxElement = 0;
+for(let i=0;i<array.length;i++){
+  if ( maxElement < array[i] ){
+  maxElement = array[i];
+ 
+  }
+   console.log(array[i]);}
+   console.log("MAXELEMENT" +" "+ maxElement);
+
+
+
+ console.log ("input border of number")
+//console.log(array[i])
+//let number = prompt("input") 
+// maxElement = function findMaxElement (array){
+//    for (i=0;i<array.lenth;i++){
+//     maxElement = array[i];
+ 
+//   }
+//    console.log(array[i]);
+//    console.log("MAXELEMENT" +" "+ maxElement);
+// }
+
